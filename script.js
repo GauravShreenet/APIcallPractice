d591ba67fddaa70aa22ca6f2efc03e17e685cf4d
Gaurav Shreenet <gthakuri656@gmail.com>
2023-09-21 18:17:02 +1000

    fetching the api

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 const apiUrl = "http://api.screenshotlayer.com/api/capture?access_key=85f73396c55c387bb5bffa86214aed1e";
 
-let captureUrl = [];
 
-const fetchScreen = async (url) => {
-    try {
-        const dt = await fetch(url);
-        const data = await dt.json();
-        captureUrl = data.results;
+// let captureUrl = [];
+
+// const fetchScreen = async (url) => {
+//     try {
+//         const dt = await fetch(url);
+//         const data = await dt.json();
+//         captureUrl = data.results;
         
-    }catch{
-        console.log(error);
-    }
-}
+//     }catch{
+//         console.log(error);
+//     }
+// }
+    let str = ""
 
-console.log("http://api.screenshotlayer.com/api/capture?access_key=85f73396c55c387bb5bffa86214aed1e");
+    const screenShot = document.getElementById("captureScreen");
+    const urlText = document.getElementById("urlText").value;
 
-// document.getElementById("capture").addEventListener("click", (e) => {
-//     const {value} = e.target;
+    document.getElementById("capture").addEventListener("click", () => {
+        str = `<img src="${apiUrl}&url=${urlText}" class="img-fluid" alt="url-screenshot"></img>`
 
+        screenShot.innerHTML = str;
+    });
 
-// })
+    
